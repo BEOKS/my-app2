aa0426201a4fcba04140a998ea8db900629a9b6d
slwnskq@naver.com <slwnskq@knuprime070ui-MacBook-Pro.local>
2020-09-27 15:39:54 +0900

    implemnt getting table data method and test complete

--- Main.java
+++ Main.java
@@ -1,14 +1,30 @@
 import org.jsoup.*;
 import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
 
 public class Main{
     public static void main(String[] args) {
 	// write your code here
-
+        String url="http://computer.knu.ac.kr/06_sub/02_sub.html";
+        try {
+            while (true) {
+                Elements elements=null;
+                if((elements=getTableElements(getDocument(url)))==null){
+                    System.err.println("Fail : cannot connet to "+url);
+                }
+                else{
+                    System.out.println("Success : connet to "+url);
+                }
+                Thread.sleep(3000);
+            }
+        }
+        catch (InterruptedException e){
+            e.printStackTrace();
+        }
     }
-    public Document getDocument(String url){
+    public static Document getDocument(String url){
         Document document=null;
         try{
             document=Jsoup.connect(url).get();
@@ -17,5 +33,13 @@ public class Main{
             e.printStackTrace();
         }
         return document;
+    }
+    public static Elements getTableElements(Document document){
+        if(document==null){
+            return null;
+        }
+        else{
+            return document.getElementsByTag("table");
+        }
     }
 }
