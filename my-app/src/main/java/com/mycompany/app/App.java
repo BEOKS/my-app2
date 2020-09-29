cc59db1856289eaa3947a0c069769ced0aeaf03f
BEOKS <lee01042000@gmail.com>
2020-09-29 15:33:05 +0900

    create mvn projects and test completed

---
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,46 @@
+package com.mycompany.app;
+import org.jsoup.*;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class App{
+    public static void main(String[] args) {
+	// write your code here
+        String url="http://computer.knu.ac.kr/06_sub/02_sub.html";
+        try {
+            while (true) {
+                Elements elements=null;
+                if((elements=getTableElements(getDocument(url)))==null){
+                    System.err.println("Fail : cannot connet to "+url);
+                }
+                else{
+                    System.out.println("Success : connet to "+url);
+                }
+                Thread.sleep(3000);
+            }
+        }
+        catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+    public static Document getDocument(String url){
+        Document document=null;
+        try{
+            document=Jsoup.connect(url).get();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+        return document;
+    }
+    public static Elements getTableElements(Document document){
+        if(document==null){
+            return null;
+        }
+        else{
+            return document.getElementsByTag("table");
+        }
+    }
+}
